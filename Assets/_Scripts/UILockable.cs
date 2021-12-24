e29d3e1ad308986c2739f78e509dffc7f2d70a0a
Siem <siemgdeveloper@gmail.com>
2021-12-24 09:41:09 -0500

    Add Lockable Levels
    Hid Locked Levels with UILockLevel

---
+++ Assets/_Scripts/UILockable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class UILockable : MonoBehaviour
+{ 
+    private void OnEnable()
+    {
+        var startButton = GetComponent<UIStartLevelButton>();
+        string key = startButton.LevelName + "Unlocked"; //Level1Unlocked"
+        int unlocked = PlayerPrefs.GetInt(key, 0);
+
+        if (unlocked == 0)
+            gameObject.SetActive(false);
+    }
+
+
+}
--- Assets/_Scripts/UIStartLevelButton.cs
+++ Assets/_Scripts/UIStartLevelButton.cs
@@ -6,14 +6,11 @@ public class UIStartLevelButton : MonoBehaviour
 {
     [SerializeField] string _levelName;
 
+    //Way to grab the LevelName without having access to the actual level name!
+    public string LevelName => _levelName;
+
     public void LoadLevel()
     {
         SceneManager.LoadScene(_levelName);
-    }
-
-    private void OnValidate()
-    {
-        //If there isn't a TMP object, then the name won't be set, otherwise, name the object the Level Name!
-        GetComponentInChildren<TMP_Text>()?.SetText(_levelName);
     }
 }
