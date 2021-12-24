069eb369ab13bf5fc40958808e98a19db7d1f1de
Siem <siemgdeveloper@gmail.com>
2021-12-24 08:48:53 -0500

    Added Menu
    New Panel Graphic/Toggle
    Questionable OnValidate

--- Assets/_Scripts/UIStartLevelButton.cs
+++ Assets/_Scripts/UIStartLevelButton.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,5 +9,11 @@ public class UIStartLevelButton : MonoBehaviour
     public void LoadLevel()
     {
         SceneManager.LoadScene(_levelName);
+    }
+
+    private void OnValidate()
+    {
+        //If there isn't a TMP object, then the name won't be set, otherwise, name the object the Level Name!
+        GetComponentInChildren<TMP_Text>()?.SetText(_levelName);
     }
 }
