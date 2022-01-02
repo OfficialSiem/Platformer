a1aaa248c9eb3ad052bbb76c8cdbce21fcce18b9
Siem <siemgdeveloper@gmail.com>
2022-01-02 15:44:04 -0500

    Fireball Update
    Fireball Reads Direction of Player when instantiated
    Fireball bounces

--- Assets/_Scripts/FireBall.cs
+++ Assets/_Scripts/FireBall.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class FireBall : MonoBehaviour
 {
+    Rigidbody2D _rigidbody = null;
+
     [Tooltip("How fast the Fireball will move")]
-    private float _launchForce = 5;
+    [Range(3f, 8f)] [SerializeField] float _launchForce = 5f;
+
+    [Tooltip("Rate the fireball can bounce")]
+    [Range(3f, 8f)] [SerializeField] float _bounceForce = 5f;
+
+    public float Direction { get; set; }
+    [Tooltip("How many times can the fireball bounce")]
+    [SerializeField] int _bouncesRemaining = 3;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(_launchForce, 0);
+        _rigidbody = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        _bouncesRemaining--;
+        if (_bouncesRemaining < 0)
+            DestroyFireBall();
+        else
+            _rigidbody.velocity = new Vector2(_launchForce * Direction, _bounceForce);
+
+    }
+
+    //In The Future, I'd like to make the Fireball emit some particles when it dies (smoke, smaller fireballs, whatever)
+    void DestroyFireBall()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/_Scripts/FireballLauncher.cs
+++ Assets/_Scripts/FireballLauncher.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class FireballLauncher : MonoBehaviour
 {
+    #region Fire Ball & Shooting
     [Tooltip("The Fireballs Prefab")]
-    [SerializeField] FireBall _fireballPrefab;
-    void Start()
+    [SerializeField] FireBall _fireballPrefab = null;
+    string _fireButton;
+    private float _nextFireTime;
+    [Tooltip("At what rate should the fireball be shooting at?")]
+    [SerializeField] float _fireRate = 0.25f;
+    #endregion
+
+    //Figure out which player is firing
+    Player _player = null;
+    string _horizontalAxis;
+
+    //Used to figure out which button is being pressed
+
+
+
+
+    void Awake()
     {
-        Instantiate(_fireballPrefab, transform.position, Quaternion.identity);
+        _player = GetComponent<Player>();
+        _fireButton = $"P{_player.PlayerNumber}Fire";
+        _horizontalAxis = $"P{_player.PlayerNumber}Horizontal";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown(_fireButton) && Time.time >= _nextFireTime)
+        {
+            var horizontal = Input.GetAxis(_horizontalAxis);
+            FireBall fireball = Instantiate(_fireballPrefab, transform.position, Quaternion.identity);
+            fireball.Direction = horizontal >= 0 ? 1f : -1f;
+            _nextFireTime = Time.time + _fireRate;
+        }
+            
     }
 }
